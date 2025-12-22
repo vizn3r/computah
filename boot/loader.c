095f1b64fb2cecce471e36fed3aa3548c7e4921e
vizn3r <viznertw@gmail.com>
2025-12-22 16:49:53 +0100

    Cleaning up the mess after H3, deprecating bootloader for now

--- boot/loader.c
+++ boot/loader.c
@@ -1,22 +1,7 @@
-#include <lib/sd_mmc.h>
 #include <lib/uart.h>
 
 void loader(void) {
-  uart_print("Loading kernel:\n");
-  // do other shit
-  uart_print(" >> Init SD card\n");
-
-  init_mmc();
-  if (mmc_init_card() == -1) {
-    uart_print(" !!>> Couldn't initialize SD card\n");
-  }
-
-  uart_print(" << SD card initialized\n");
-  uart_print(" >> Reading kernel\n");
-
-  mmc_read_blocks(2048, (void *)0x40000000, 100);
-  uart_print(" >> Jumping to kernel\n");
-
-  void (*kernel_main)(void) = (void (*)(void))0x40000000;
-  kernel_main();
+  uart_print("loader.c not implemented");
+  // for now, this does nothing. bootloader coming later
+  return;
 }
